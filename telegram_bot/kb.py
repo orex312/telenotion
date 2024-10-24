751130a0f200aea02a83a3692750ea3c1546bc58
TFolkner <you@example.com>
2024-10-25 01:10:05 +0300

    clear trash

--- telegram_bot/kb.py
+++ telegram_bot/kb.py
@@ -6,6 +6,6 @@ menu = [
 
 ]
 
-menu = keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+menu = InlineKeyboardMarkup(inline_keyboard=menu)
 exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
 iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
