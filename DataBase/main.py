4567d6be966b5ae9c873720c2cab23ad26e47bce
Aleksandr Oreshkin <saskaorex@gmail.com>
2024-10-24 18:34:51 +0300

    rename to user_operations

--- DataBase/main.py
+++ DataBase/main.py
@@ -1,12 +1,25 @@
 from connection import Connection
-from basic_operations import *
+from DataBase.user_operations import *
 
 user_name = "Alex"
-User_login = "Apelsin312"
-resp = addNewUser(User_login, user_name)
+user_login = "Apelsin312"
+
+# Добавить нового юзера
+resp = addNewUser(user_login, user_name)
 if resp:
     print(resp)
-resp = getAllUsers()[0]
-#print(resp[0])
+
+# Список всех юзеров
+resp = getAllUsers()
+for i in resp:
+    print(f'Имя: {i["user_name"]}, логин: {i["login"]}, дата создания: {i["creatad_at"]}')
+
+# Юзер по ID
+resp = getUserById(1)
+for i in resp:
+    print(f'Имя: {i["user_name"]}, логин: {i["login"]}, дата создания: {i["creatad_at"]}')
+
+# Юзер по логину
+resp = getUserByLogin(user_login)
 for i in resp:
     print(f'Имя: {i["user_name"]}, логин: {i["login"]}, дата создания: {i["creatad_at"]}')
