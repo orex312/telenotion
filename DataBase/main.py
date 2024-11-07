8bd2d9778d2448816383fd0385945b310d52a368
TFolkner <you@example.com>
2024-11-07 22:10:13 +0300

    исправления в bd

--- DataBase/main.py
+++ DataBase/main.py
@@ -3,12 +3,17 @@ from user_operations import *
 
 user_name = "Ilya"
 user_login = "Pipiska"
-'''
+
 # Добавить нового юзера
 resp = addNewUser(user_login, user_name)
 if resp:
     print(resp)
-    
+
+# Список всех юзеров
+resp = getAllUsers()
+for i in resp:
+    print(f'Имя: {i["user_name"]}, логин: {i["login"]}, дата создания: {i["creatad_at"]}')
+
 # Юзер по ID
 resp = getUserById(1)
 for i in resp:
@@ -18,10 +23,3 @@ for i in resp:
 resp = getUserByLogin(user_login)
 for i in resp:
     print(f'Имя: {i["user_name"]}, логин: {i["login"]}, дата создания: {i["creatad_at"]}')
-'''
-
-# Список всех юзеров
-resp = getAllUsers()
-for i in resp:
-    print(f'Имя: {i["user_name"]}, логин: {i["login"]}, дата создания: {i["creatad_at"]}')
-
