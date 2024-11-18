bdf69ef4d7b8af1782bbfd930464a2d3287545b5
TFolkner <you@example.com>
2024-11-19 00:19:03 +0300

    save commit

--- telegram_bot/dialog_with_user.py
+++ telegram_bot/dialog_with_user.py
@@ -24,35 +24,13 @@ API_TOKEN = read_data ("bot_info.txt")
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-async def send_photo_echo(message: Message):
-    print ("================================================================================================================PHOTO")
-    print(message.model_dump_json(indent=4, exclude_none=True))
-    await message.reply_photo(message.photo[0].file_id)
 
-
-async def send_sticker_echo(message: Message):
-    print ("================================================================================================================STICKER")
-    print(message.model_dump_json(indent=4, exclude_none=True))
-    await message.reply_sticker (message.sticker.file_id)
-
-
-async def send_text_echo(message: Message):
-    print ("================================================================================================================TEXT")
-    print(message.model_dump_json(indent=4, exclude_none=True))
-    await message.reply (message.text)
-
-
-async def send_echo(message: Message):
+async def send_any_echo(message: Message):
     print ("================================================================================================================ANY")
     print(message.model_dump_json(indent=4, exclude_none=True))
+    await message.send_copy (chat_id = message.chat.id)
     
-
-
-
-dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
-dp.message.register(send_sticker_echo, F.content_type == ContentType.STICKER)
-dp.message.register(send_text_echo, F.content_type == ContentType.TEXT)
-dp.message.register(send_echo)
+dp.message.register(send_any_echo)
 
 # Она запускает поллинг, то есть постоянный опрос сервера Telegram на наличие апдейтов для бота
 if __name__ == '__main__':
