eaa05e61c47a5058c9edae333ab3515ae4c20a0a
TFolkner <you@example.com>
2024-11-21 01:20:38 +0300

    нахер json просто config.py проверил тестовую оплату, работает, надо тр=еперь попробовать со звездами

--- telegram_bot/dialog_with_user.py
+++ telegram_bot/dialog_with_user.py
@@ -1,55 +1,53 @@
 # попробуем иначе
+import telegram_bot.bot_config as bot_config
 import logging
-import requests
-import json
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.types import ContentType
 from aiogram import F
 
 
-
 class BOT:
 	def __init__(self, BOT_TOKEN, URL, MAX_COUNTER):
-          self.BOT_TOKEN = BOT_TOKEN
-          self.URL = URL
-          self.MAX_COUNTER = MAX_COUNTER
-
-
-# чтение даты из json файла
-def read_json_data (sFile_json_data):
-    with open (sFile_json_data, 'r') as json_file:
-         data = json.load (json_file)
-    return data
-bot_data = read_json_data ('bot_info.json')
-
-API_TOKEN = bot_data["bot_token"]
-
-
+		self.BOT_TOKEN = BOT_TOKEN
+		self.URL = URL
+		self.MAX_COUNTER = MAX_COUNTER
 
 # Создаем объекты бота и диспетчера
-bot = Bot(token=API_TOKEN)
+bot = Bot(bot_config.bot_token)
 dp = Dispatcher()
 
-tg_api = 'https://api.telegram.org/bot'
-@dp.message (Command(commands=["weather"]))
-async def send_weather (message: Message):
-      api_url = 'https://api.telegram.org/bot7767052229:AAGcy1tK09SyCAXXz17Uso41WSYQqD-RxRM/sendMessage?chat_id=235503146&text=Привет, Mikhail!'
-      req_url = f'{api_url}{BOT}/'
-      resp = requests.get (api_url)
-      await message.reply (resp.text)
-      return 0
+# ========================================================================================================= описание прайса
+PRICE = types.LabeledPrice (label = "Подписка на месяц", amount = 500*100) # amount сумма в копейках
+
+# ============================================================================================================= обработчики
+@dp.message (Command("pay"))
+async def buy_month (message: types.Message):
+	if bot_config.pay_token.split(':')[1] == 'TEST':
+		await bot.send_message(message.chat.id, "Попробуем оплатить")
+    
+	await bot.send_invoice(message.chat.id,
+                           title="Подписка на бота",
+                           description="Активация подписки на бота на 1 месяц",
+                           provider_token=bot_config.pay_token,
+                           currency="rub",
+                           is_flexible=False,
+                           prices=[PRICE],
+                           start_parameter="one-month-subscription",
+                           payload="test-invoice-payload")
 
 @dp.message (Command("json"))
-async def send_text_echo(message: Message):
+async def send_reply_json(message: types.Message):
     print(message.model_dump_json(indent=4, exclude_none=True)) # строка исключительно для тестов
     await message.reply (message.model_dump_json(indent=4, exclude_none=True))
+    return 0
+# или такая регистрация обработчика
+#dp.message.register (send_reply_json, F.content_type == ContentType.TEXT)
 
-#dp.message.register(send_weather, F.content_type == ContentType.TEXT)
+# =========================================================================================================================
 
 # Она запускает поллинг, то есть постоянный опрос сервера Telegram на наличие апдейтов для бота
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     dp.run_polling(bot)
-
