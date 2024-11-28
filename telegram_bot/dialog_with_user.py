bfc144c0dd965522eee9fee285eaa1d29fb5edbe
TFolkner <you@example.com>
2024-11-29 02:09:37 +0300

    добавлена тестовая обработка оплаты рублями

--- telegram_bot/dialog_with_user.py
+++ telegram_bot/dialog_with_user.py
@@ -1,38 +1,82 @@
-# попробуем иначе
+from aiogram import Bot, Dispatcher, Router, types, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import CallbackQuery, Message, User, ContentType
+from aiogram_dialog import Dialog, DialogManager, StartMode, Window, setup_dialogs
+from aiogram_dialog.widgets.kbd import Button, Row, Column, SwitchTo
+from aiogram_dialog.widgets.text import Const, Format, Case, List
 import bot_config
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from aiogram.types import Message
-from aiogram.types import ContentType
-from aiogram import F
+import text
+import random
 
 
-class BOT:
-	def __init__(self, BOT_TOKEN, URL, MAX_COUNTER):
-		self.BOT_TOKEN = BOT_TOKEN
-		self.URL = URL
-		self.MAX_COUNTER = MAX_COUNTER
 
-# Создаем объекты бота и диспетчера
-bot = Bot(bot_config.bot_token)
+
+BOT_TOKEN = bot_config.main_bot_token
+
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+router = Router()
 
-# ========================================================================================================= описание прайса
+
+class MainDialog(StatesGroup):
+	start = State()
+	all_welcome = State()
+	donate = State()
+	
+
+
+
+
+
+
+# обработка доната рублями
+async def donate_rubs(callback: CallbackQuery, widget: Button, dialog_manager: DialogManager):
+	await callback.message.edit_text(text='Донат рублями 💵 по карточке')
+	await dialog_manager.done()  
+
+
+# обработка доната звездами
+async def donate_stars (callback: CallbackQuery, widget: Button, dialog_manager: DialogManager):
+	await callback.message.edit_text (text='Донат звездами ⭐️ телеграм')
+	await dialog_manager.done()
+
+
+async def show_all_welcome (callback: CallbackQuery, widget: Button, dialog_manager: DialogManager):
+	await dialog_manager.start(MainDialog.all_welcome)
+
+
+async def donate_window (callback: CallbackQuery, widget: Button, dialog_manager: DialogManager):
+	await dialog_manager.start (MainDialog.donate)
+
+
+
+
+
+
+
+
+
+# Это классический хэндлер, который будет срабатывать на команду /start
+@router.message(Command("start"))
+async def command_start_process(message: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(state=MainDialog.start, mode=StartMode.RESET_STACK)
+
+
 PRICE = types.LabeledPrice (label = "Подписка на месяц", amount = 10 * 100) #копейки
-
-# ============================================================================================================= обработчики
-@dp.message (Command("pay"))
-async def buy_month (message: types.Message):
-	if bot_config.pay_token.split(':')[1] == 'TEST':
-		await bot.send_message(message.chat.id, "Попробуем оплатить")
+async def donate_rubs_done (callback: CallbackQuery, widget: Button, dialog_manager: DialogManager):
+	message = callback.message
+	#if bot_config.main_pay_token.split(':')[1] == 'TEST':
+	#	await bot.send_message(message.chat.id, "Попробуем оплатить")
     
 	await bot.send_invoice(message.chat.id,
                            title="Подписка на бота",
                            description="Активация подписки на бота на 1 месяц",
-                           provider_token=bot_config.pay_token,
+                           provider_token=bot_config.main_pay_token,
                            currency="rub",
-						   #photo_url="../photo_2024-11-21_10-32-46.jpg",
                            is_flexible=False,
                            prices=[PRICE],
                            start_parameter="one-month-subscription",
@@ -43,29 +87,110 @@ async def buy_month (message: types.Message):
 async def pre_checkout (pre_checkout_q: types.PreCheckoutQuery):
 	await bot.answer_pre_checkout_query (pre_checkout_q.id, ok=True)
 
+
 # обработка успешного платежа
-async def succesful_payment (message: types.Message):
+@router.message (F.content_type == ContentType.SUCCESSFUL_PAYMENT)
+async def succesful_payment (message: types.Message, dialog_manager: DialogManager):
 	print ("Succesful payment:")
 	payment_info = message.successful_payment
-	await message.reply (payment_info.model_dump_json(indent=4, exclude_none=True))
+	#await message.reply (payment_info.model_dump_json(indent=4, exclude_none=True))
 	await bot.send_message (message.chat.id, 
 						f"Платеж на сумму {message.successful_payment.total_amount // 100} {message.successful_payment.currency} прошел успешно!!!")
-@dp.message.register  (succesful_payment, F.content_type == ContentType.SUCCESSFUL_PAYMENT)
+	await dialog_manager.start(state=MainDialog.start, mode=StartMode.RESET_STACK)
+
+	
 
 
 
-# тестилка
-@dp.message (Command("json"))
-async def send_reply_json(message: types.Message):
-    print(message.model_dump_json(indent=4, exclude_none=True)) # строка исключительно для тестов
-    await message.reply (message.model_dump_json(indent=4, exclude_none=True))
-    return 0
-# или такая регистрация обработчика
-#dp.message.register (send_reply_json, F.content_type == ContentType.TEXT)
 
-# =========================================================================================================================
 
-# Она запускает поллинг, то есть постоянный опрос сервера Telegram на наличие апдейтов для бота
+
+
+
+
+
+async def get_username (event_from_user: User, **kwargs):
+	return {'username': event_from_user.username}
+
+async def welcome_getter (event_from_user: User, **kwargs):
+	return {'username': event_from_user.username, 'number': random.randint(1, 5)}
+
+async def get_welcome_words (event_from_user: User, **kwargs):
+	username = event_from_user.username
+	return {'items': (
+		(1, text.random_welcome_1.format(username = username)),
+		(2, text.random_welcome_2.format(username = username)),
+		(3, text.random_welcome_3.format(username = username)),
+		(4, text.random_welcome_4.format(username = username)),
+		(5, text.random_welcome_5.format(username = username)),
+	)}
+
+
+
+
+
+
+
+
+
+start_dialog = Dialog(
+	Window (
+		Case (
+			texts={
+				1: Format (text=text.random_welcome_1),
+				2: Format (text=text.random_welcome_2),
+				3: Format (text=text.random_welcome_3),
+				4: Format (text=text.random_welcome_4),
+				5: Format (text=text.random_welcome_5),
+			},
+			selector='number',
+		),
+		Column (
+			Button (text=Const(text.main_window_my_info), id = text.main_window_my_info_id),
+			Button (text=Const(text.main_window_my_tasks), id = text.main_window_my_tasks_id),
+			Button (text=Const(text.main_window_new_tasks), id = text.main_window_new_tasks_id),
+			Button (text=Const(text.main_window_donate), id = text.main_window_donate_id, on_click = donate_window),
+			Button (text=Const('Показать список приветствий'), id = 'welcome_list', on_click=show_all_welcome),
+		),
+		getter=welcome_getter,
+		state=MainDialog.start,
+	),
+	Window (
+		Format (text='Список приветствий'),
+		List (
+			field=Format('{item[0]}.  {item[1]}'),
+			items = 'items'),
+		SwitchTo (Const('back'), id='back', state=MainDialog.start),
+		getter = get_welcome_words,
+		state = MainDialog.all_welcome,
+	),
+	Window (
+		Format (text=text.donate_word),
+		Column (
+			Button (Const(text.donate_stars), id = text.donate_stars_id),
+			Button (Const(text.donate_rubs), id = text.donate_rubs_id, on_click=donate_rubs_done),
+		),
+		SwitchTo (Const('back'), id='back', state=MainDialog.start),
+		getter=get_username,
+		state = MainDialog.donate,
+	),
+
+)
+
+
+#@dp.message.register (succesful_payment, F.content_type == ContentType.SUCCESSFUL_PAYMENT)
+
+
+def test():
+	return 0
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    dp.run_polling(bot)
+	logging.basicConfig(level=logging.INFO)
+	dp.include_router(router)
+	dp.include_router(start_dialog)
+	setup_dialogs(dp)
+	dp.run_polling(bot)
+
+
+
+
--- telegram_bot/text.py
+++ telegram_bot/text.py
@@ -1,10 +1,35 @@
-# Файл с тестовыми сообщениями для всего проекта целиком (всё берется из одного места, мб потом разобью)
+# Format
+welcome_first_words = 'Привет, {username}!\nЯ твой личный бот для заметок.\nДавай начнём'
+welcome_words = 'Привет, {username}!'
+donate_word = '{username}, рады, что ты решил поддержать нас'
 
-text_tasks_list = "Вот список твоих задач"
-text_new_task = "Новая задача"
 
-def main_menu (user_name):
-    return "Привет, {name}, чем могу помочь ?".format(name = user_name)
+# Const
+main_window_my_tasks = 'Мои задачи'
+main_window_my_tasks_id = 'my_tasks'
 
-def welcom_text (user_name):
-    return "Приветствую, я бот для заметок, начнём работу".format(name = user_name)
+main_window_new_tasks = 'Новая задача'
+main_window_new_tasks_id = 'new_task'
+
+main_window_my_info = 'Статус'
+main_window_my_info_id = 'my_info_status'
+
+main_window_donate = 'Донат'
+main_window_donate_id = 'donate'
+
+back_word = 'Назад'
+back_word_id = 'back_word_id'
+
+donate_rubs = 'Поддержать рублями 💴'
+donate_rubs_id = 'donate_rubs_id'
+donate_stars = 'Поддержать звездами телеграм⭐️'
+donate_stars_id = 'donate_stars_id'
+
+
+# приветственный текст главного меню
+random_welcome_1 = '{username}, Дарова заебал'
+random_welcome_2 = 'Привет, {username}!'
+random_welcome_3 = 'Чё каво, {username}'
+random_welcome_4 = 'Есть два стула, {username}'
+random_welcome_5 = 'Заходят как-то {username} и хохол в бар'
+
