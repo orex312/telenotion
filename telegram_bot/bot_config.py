24da01659ef4a57326745776cc58299f70eaecc6
TFolkner <you@example.com>
2024-11-29 03:01:18 +0300

    add config

---
+++ telegram_bot/bot_config.py
@@ -0,0 +1,4 @@
+main_bot_token = "7767052229:AAGcy1tK09SyCAXXz17Uso41WSYQqD-RxRM"
+main_pay_token = "1744374395:TEST:0e0e3b1ee44e10f4383f"
+
+test_bot_token = "8046199610:AAGaVMYvVtXxs7rtg2JLEMdTD3H2as74Tow"
