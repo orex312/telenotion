db15812261b3f373bb965592ffba53d4506d52c4
TFolkner <you@example.com>
2024-12-01 23:33:16 +0300

    Добавил часть текстовых заготовок

--- telegram_bot/text.py
+++ telegram_bot/text.py
@@ -8,3 +8,65 @@ def main_menu (user_name):
 
 def welcom_text (user_name):
     return "Приветствую, я бот для заметок, начнём работу".format(name = user_name)
+
+
+# Format
+welcome_first_words = 'Привет, {username}!\nЯ твой личный бот для заметок.\nДавай начнём'
+welcome_words = 'Привет, {username}!'
+donate_word = '{username}, рады, что ты решил поддержать нас'
+
+
+# Const
+main_window_my_tasks = 'Мои задачи'
+main_window_my_tasks_id = 'my_tasks'
+
+main_window_new_tasks = 'Новая задача'
+main_window_new_tasks_id = 'new_task'
+
+main_window_my_info = 'Статус'
+main_window_my_info_id = 'my_info_status'
+
+main_window_donate = 'Донат'
+main_window_donate_id = 'donate'
+
+back_word = 'Назад'
+back_word_id = 'back_word_id'
+
+donate_rubs = 'Поддержать рублями 💴'
+donate_rubs_id = 'donate_rubs_id'
+donate_stars = 'Поддержать звездами телеграм⭐️'
+donate_stars_id = 'donate_stars_id'
+
+
+
+
+# приветственный текст главного меню
+random_welcome_1 = '{username}, Дарова заебал'
+random_welcome_2 = 'Привет, {username}!'
+random_welcome_3 = 'Чё каво, {username}'
+random_welcome_4 = 'Есть два стула, {username}'
+random_welcome_5 = 'Заходят как-то {username} и хохол в бар'
+
+# ==================================================================================================
+# Format ==========================================================
+name_format_text = 'Название: <b>{title}</b>'
+discription_format_text = 'Описание: {description}'
+
+
+# buttons ==========================================================
+menu_button = 'Меню📖'
+change_button = 'Изменить'
+notification_buttn = 'Напоминание🕘'
+delete_button = 'Удалить ❌'
+back_button = 'Назад↩️'
+done_button = 'Готово ✅'
+add_discription_button = 'Изменить название'
+cancel_button = 'Отмена❌'
+change_taskname_button = 'Изменить название✏️'
+change_task_discription_button = 'Изменить описание✏️'
+change_date_button = 'Изменить дату📆'
+change_time_button = 'Изменить время🕘'
+save_button = 'Сохранить✅'
+change_date_notification_button = 'Измените дату уведомления:'
+choose_time_notification_button = 'Выберите дату уведомления:'
+
